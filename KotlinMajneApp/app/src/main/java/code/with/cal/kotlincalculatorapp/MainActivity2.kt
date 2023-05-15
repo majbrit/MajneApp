e981d8845fb2db652b7283321ad6ad87d1aba899
majbrit <majbrit.schoettner@yahoo.de>
2023-05-15 15:34:05 +0200

    einheitliche Buttons

--- KotlinMajneApp/app/src/main/java/code/with/cal/kotlincalculatorapp/MainActivity2.kt
+++ KotlinMajneApp/app/src/main/java/code/with/cal/kotlincalculatorapp/MainActivity2.kt
@@ -1,7 +1,11 @@
 package code.with.cal.kotlincalculatorapp
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity2.*
@@ -11,6 +15,18 @@ class MainActivity2  : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        supportActionBar?.hide()
     }
 
     fun backAction(view: View)
