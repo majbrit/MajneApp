1fd4e6ce8bedaa4adea56201250371615139573c
majbrit <majbrit.schoettner@yahoo.de>
2023-05-20 18:54:40 +0200

    animation

--- app/src/main/java/code/with/cal/kotlincalculatorapp/Animation.kt
+++ app/src/main/java/code/with/cal/kotlincalculatorapp/Animation.kt
@@ -1,11 +1,17 @@
 package code.with.cal.kotlincalculatorapp
 
+import android.R.attr.animation
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.animation.*
+
 
 class Animation : AppCompatActivity() {
 
@@ -23,4 +29,57 @@ class Animation : AppCompatActivity() {
         }
     }
 
+    fun myanimAction(view: View)
+    {
+        if (view is Button)
+        {
+            /*
+            var animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fadein);
+            catImage.startAnimation(animation1);*/
+
+            val animator = ValueAnimator.ofFloat(0f, 200f)
+            animator.addUpdateListener {
+                val value = it.animatedValue as Float
+                catImage.translationX = value
+                catImage.alpha = 1.0f - value/200f
+            }
+
+            val animator2 = ValueAnimator.ofFloat(0f, 200f)
+            animator2.addUpdateListener {
+                val value = it.animatedValue as Float
+                catImage.translationX = 200f - value
+                catImage.alpha = value/200f
+            }
+
+            animator.addListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {
+                    // 3
+                    Toast.makeText(applicationContext, "Cat took off", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    // 4
+                    Toast.makeText(applicationContext, "Cat is on the moon", Toast.LENGTH_SHORT)
+                        .show()
+                    animator2.duration = 5000L
+                    animator2.start()
+                    //finish()
+                }
+
+                override fun onAnimationCancel(animation: Animator) {}
+
+                override fun onAnimationRepeat(animation: Animator) {}
+            })
+
+            animator.duration = 5000L
+            animator.start()
+
+
+        }
+    }
 }
+
+
+
